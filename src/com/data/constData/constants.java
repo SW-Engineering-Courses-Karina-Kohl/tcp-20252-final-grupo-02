52d44a620e34343501f1194dce290e82f06a3b5a
Leonardo Leites <leoleites2002@gmail.com>
2025-11-04 22:48:35 -0300

    add mockup and colors

---
+++ src/com/data/constData/constants.java
@@ -0,0 +1,18 @@
+package com.data.constData;
+import java.awt.*;
+
+/*  
+
+cores (transparencia 70%)
+vermelho sair e grupo encerrado - #ffc9c9
+amarelo campos de informação - #ffec99
+verde grupo votação encerrada e infos do grupo - #b2f2bb
+azul botoes - #a5d8ff 
+
+*/
+public class constants {
+    public static final Color YELLOW = new Color(255, 236, 153); // #ffec99
+    public static final Color RED = new Color(255,  201, 201); // #ffc9c9
+    public static final Color GREEN = new Color(178,  242, 187); // #b2f2bb
+    public static final Color BLUE = new Color(165,  216, 255); // #a5d8ff
+}
