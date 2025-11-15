97da62783d9c84ff501154b00f099201468b463a
Matheus Candiotto <matheus.candiotto@ufrgs.br>
2025-11-15 15:26:53 -0300

    Primeira implementação de logging
    Troca da biblioteca utilizada para o sistema de logging (que agora é funcional).

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.ArrayList;
+import org.tinylog.Logger;
 
 public class Main {
 
@@ -12,6 +13,8 @@ public class Main {
 	
 	public ArrayList<Book> getBooks() {
 		
+		Logger.info("Lista de livros retornada com sucesso");
+		
 		return books;
 		
 	}
@@ -20,15 +23,21 @@ public class Main {
 		
 		books.add(newBook);
 		
+		Logger.info("Livro criado com sucesso");
+		
 	}
 	
 	public void deleteBook(Book book) {
 		
 		books.remove(book);
 		
+		Logger.info("Livro excluído com sucesso");
+		
 	}
 	
 	public ArrayList<BookClub> getBookClubs() {
+		
+		Logger.info("Lista de clubes do livro retornada com sucesso");
 		
 		return bookClubs;
 		
@@ -38,15 +47,21 @@ public class Main {
 		
 		bookClubs.add(newBookClub);
 		
+		Logger.info("Clube do livro criado com sucesso");
+		
 	}
 	
 	public void deleteBookClub(BookClub bookClub) {
 		
 		bookClubs.remove(bookClub);
 		
+		Logger.info("Clube do livro excluído com sucesso");
+		
 	}
 	
 	public ArrayList<Meeting> getMeetings() {
+		
+		Logger.info("Lista de encontros retornada com sucesso");
 		
 		return meetings;
 		
@@ -56,15 +71,21 @@ public class Main {
 		
 		meetings.add(newMeeting);
 		
+		Logger.info("Encontro criado com sucesso");
+		
 	}
 	
 	public void deleteMeeting(Meeting meeting) {
 		
 		meetings.remove(meeting);
 		
+		Logger.info("Encontro excluído com sucesso");
+		
 	}
 
 	public ArrayList<Poll> getPolls() {
+		
+		Logger.info("Lista de votações retornada com sucesso");
 		
 		return polls;
 		
@@ -74,15 +95,21 @@ public class Main {
 		
 		polls.add(newPoll);
 		
+		Logger.info("Votação criada com sucesso");
+		
 	}
 	
 	public void deletePoll(Poll poll) {
 		
 		polls.remove(poll);
 		
+		Logger.info("Votação excluída com sucesso");
+		
 	}
 	
 	public ArrayList<User> getUsers() {
+		
+		Logger.info("Lista de usuários retornada com sucesso");
 		
 		return users;
 		
@@ -92,17 +119,21 @@ public class Main {
 		
 		users.add(newUser);
 		
+		Logger.info("Usuário criado com sucesso");
+		
 	}
 	
 	public void deleteUser(User user) {
 		
 		users.remove(user);
 		
+		Logger.info("Usuário excluído com sucesso");
+		
 	}
 	
 	public static void main(String[] args) {
 
-		// ...
+		Logger.info("Teste de diretório");
 
 	}
 
